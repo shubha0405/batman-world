cf842ce14bbb9146cde70cc9a06a7608a3675b33
Shubha Ashok <shubha.ashokk@gmail.com>
2026-07-03 13:29:42 +0530

    Fix tsParticles dependencies

--- src/components/RainEffect.jsx
+++ src/components/RainEffect.jsx
@@ -1,5 +1,5 @@
-import Particles from "react-tsparticles";
-import { loadSlim } from "tsparticles-slim";
+import Particles from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
 
 function RainEffect() {
 
